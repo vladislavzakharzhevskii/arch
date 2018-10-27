3797d0ac92b6f41c7b77f77f5be9105f8d68d72a
Vlad_Zakharzhevskyi <vladuxa.70@gmail.com>
2018-10-27 11:47:26 +0300

    Initial commit

---
+++ src/main/java/com/vlad/home/workup/WebApplicationStartUpEntryPoint.java
@@ -0,0 +1,13 @@
+package com.vlad.home.workup;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationStartUpEntryPoint {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationStartUpEntryPoint.class, args);
+    }
+}
